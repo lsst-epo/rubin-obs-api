5b0e901a77bd97b7426cd6fb9b6c9e5af8db491f
Blake Mason <bmason@lsst.org>
2023-02-21 09:52:53 -0800

    [C] Craft 4 composer upgrades

--- api/config/general.php
+++ api/config/general.php
@@ -30,10 +30,6 @@ return [
         // The secure key Craft will use for hashing and encrypting data
         'securityKey' => App::env('SECURITY_KEY'),
 
-        // Whether to save the project config out to config/project.yaml
-        // (see https://docs.craftcms.com/v3/project-config.html)
-        'useProjectConfigFile' => true,
-
         'aliases' => [
             '@previewUrlFormat' => App::env('ALIAS_PREVIEW_URL_FORMAT'),
             '@assetsAssetVariantBaseURL' => sprintf(
