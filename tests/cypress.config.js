db90d17c87ea31482db31054dc255873699d842d
Eric Rosas <ericdrosas@gmail.com>
2023-05-23 15:48:16 -0700

    Added cypress tests, workflow, jobs, steps EPO-7941

---
+++ tests/cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("cypress");
+require('dotenv').config({ path: './.env' });
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  env: {
+    TEST_USERNAME: process.env.TEST_USERNAME,
+    TEST_PASSWORD: process.env.TEST_PASSWORD,
+    CRAFT_DASHBOARD_URL: process.env.CRAFT_DASHBOARD_URL
+  }
+});
