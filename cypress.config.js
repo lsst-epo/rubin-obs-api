5e9be27d401bab94fdb2a9a58741431f80087dab
Blake Mason <bmason@lsst.org>
2023-07-11 10:40:05 -0700

    [C] Add eslint & prettier
     - lint & fix cypress tests and associated config files

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: [
-      "tests/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}"
-    ],
+    specPattern: ["tests/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}"],
     supportFile: "tests/cypress/support/e2e.js",
     baseUrl: "http://localhost:8080/admin",
   },
-  video: false
+  video: false,
 });
